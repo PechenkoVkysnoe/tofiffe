99de3898a8f3c2ccdb6ccb273b2fb423512f52a1
Ilya Lazuk <i.lazuk45@gmail.com>
2023-12-03 17:25:47 +0300

    init commit

---
+++ bank_system/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from accounts import views
+
+urlpatterns = [
+	path('', views.index, name='index'),
+    path('login/', views.sign_in, name='login'),
+    path('logout/', views.sign_out, name='logout'),
+    path('register/', views.sign_up, name='register'),
+]
